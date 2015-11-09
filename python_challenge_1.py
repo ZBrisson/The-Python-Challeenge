75111dc0590e0825645daa77280606b75dc3a0c2
Zach Brisson <github@zachbrisson.com>
2015-11-09 16:55:03 -0500

    New Challenge
    Will revisit old challenges after 1-3 months and see if code is
    readable.

---
+++ python_challenge_1.py
@@ -0,0 +1,10 @@
+# The python challenge
+# Challenge #1
+# By: Zach Brisson
+# Description: This program solves what is called a "Caesar ciper" aka a = c, b = d, etc.
+# Reference Material that was consulted for this project can be found below:
+# https://inventwithpython.com/chapter14.html
+# 
+key = raw_input("Hello there. I am dubbed the Caesar Whisperer version .01. \nIf you know the key please enter it below, it should be a number 1-26: \n> ")
+
+print "%s" % key
